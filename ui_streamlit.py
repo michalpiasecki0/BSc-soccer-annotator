e41e62752696d1fed80c9e5fa429c38837daf09d
kraszewskik <konstanty.kraszewski.stud@pw.edu.pl>
2022-12-07 12:01:16 +0100

    update ui_streamlit.py

--- ui_streamlit.py
+++ ui_streamlit.py
@@ -6,144 +6,162 @@ from PIL import Image
 from streamlit_player import st_player
 from youtube_dl import YoutubeDL
 import cv2
+from base64 import b64encode
+from tempfile import NamedTemporaryFile
 
 # streamlit configs
-st.set_page_config(layout="wide")
+st.set_page_config(
+    page_title='Soccer annotator',
+    page_icon=':soccer:',
+    layout="wide"
+)
 
-default_video = "https://youtu.be/CmSKVW1v0xM"
-
-# title
 st.title('BSc Soccer Annotator')
 
-# video
-firstRow = st.columns([1, 1, 1])
+firstRow = st.columns([3, 4, 3])
 with firstRow[0]:
-    videoURL = st.text_input('',
-                             value=default_video,
-                             placeholder='Enter YouTube URL')
+    videoSourceType = st.radio('Video Source',
+                               ['URL', 'File'],
+                               horizontal=True)
 
+
+    def video_on_change():
+        if 'capturedVideo' in st.session_state:
+            del st.session_state['capturedVideo']
+
+
+    if videoSourceType == 'URL':
+        default_video = "https://www.youtube.com/watch?v=muIp6hciYl8"
+        videoURL = st.text_input('Enter your video URL',
+                                 value=default_video,
+                                 placeholder='Enter YouTube URL',
+                                 on_change=video_on_change)
+
+    elif videoSourceType == 'File':
+        videoFile = st.file_uploader('Upload your video file',
+                                     type='mp4',
+                                     on_change=video_on_change)
+        if videoFile is not None:
+            videoBytes = videoFile.getvalue()
+            data = b64encode(videoBytes).decode()
+            mime = "video/mp4"
+            videoURL = [{"type": mime, "src": f"data:{mime};base64,{data}"}]
+        else:
+            st.stop()
+
+    videoHeight = 500
     videoPlayer = st_player(url=videoURL,
-                            events=['onProgress'],
-                            key='video')
+                            events=['onProgress', 'onPause'],
+                            key='video',
+                            height=videoHeight)
 
-    secondsOfVideoPlayed = videoPlayer[1]['playedSeconds'] if videoPlayer[1] is not None else videoPlayer[1]
+    secondsOfVideoPlayed = videoPlayer[1]['playedSeconds'] if videoPlayer[1] is not None else 0
+
+with firstRow[1]:
+    annotationType = st.selectbox('Choose annotation type',
+                                  ['Event annotation',
+                                   'Field annotation',
+                                   'Line annotation',
+                                   'Object annotation'],
+                                  index=0)
+    if annotationType == 'Field annotation':
+        canvasDrawingMode = 'polygon'
+        annotationsFile = 'data/field_annotations.csv'
+    elif annotationType == 'Line annotation':
+        canvasDrawingMode = 'line'
+        annotationsFile = 'data/line_annotations.csv'
+    elif annotationType == 'Object annotation':
+        canvasDrawingMode = 'rect'
+        annotationsFile = 'data/object_annotations.csv'
+    elif annotationType == 'Event annotation':
+        annotationsFile = 'data/event_annotations.csv'
+    annotations = pd.read_csv(annotationsFile)
 
 with firstRow[2]:
-    canvas_field = st_canvas(
-        fill_color="rgba(255, 165, 0, 0.3)",  # Fixed fill color with some opacity
-        background_image=Image.open('data/football_pitch.png'),
-        update_streamlit=True,
-        drawing_mode='point',
-        key="canvas_pitch",
-        point_display_radius=0.3
-    )
+    if annotationType == 'Line annotation':
+        canvas_field = st_canvas(
+            fill_color="rgba(255, 165, 0, 0.3)",  # Fixed fill color with some opacity
+            background_image=Image.open('data/football_pitch.png'),
+            update_streamlit=True,
+            drawing_mode='point',
+            key="canvas_pitch",
+            point_display_radius=0.3
+        )
 
-    # debug
-    if canvas_field.json_data is not None:
-        objects = pd.json_normalize(canvas_field.json_data["objects"])  # need to convert obj to str because PyArrow
-        for col in objects.select_dtypes(include=['object']).columns:
-            objects[col] = objects[col].astype("str")
-        if len(objects) > 0:
-            st.dataframe(objects.loc[:, ['left', 'top']])
-            st.dataframe(objects)
+        # debug
+        # if canvas_field.json_data is not None:
+        #     objects = pd.json_normalize(canvas_field.json_data["objects"])
+        #     for col in objects.select_dtypes(include=['object']).columns:
+        #         objects[col] = objects[col].astype("str")
+        #     if len(objects) > 0:
+        #         st.dataframe(objects.loc[:, ['left', 'top']])
+        #         st.dataframe(objects)
 
-# table
-st.subheader('Tables')
-secondRow = st.columns([1, 2, 1, 1, 6])
+    presentedAnnotations = st.dataframe(annotations)
+
+with st.expander('Editing', expanded=False):
+    secondRow = st.columns([1, 2, 1, 1, 6])
 with secondRow[0]:
     players = pd.read_csv('data/players.csv')
     players.set_index('_')
     teams = pd.DataFrame({'Teams': players.columns[[1, 2]]})
     gb_teams = GridOptionsBuilder.from_dataframe(teams)
     gb_teams.configure_default_column(editable=True)
-    gb_teams.configure_selection(selection_mode='single',
-                                 use_checkbox=True,
-                                 pre_selected_rows=[0],
-                                 suppressRowDeselection=True)
 
     ag_teams = AgGrid(teams,
                       gridOptions=gb_teams.build(),
-                      fit_columns_on_grid_load=True,
-                      allow_unsafe_jscode=True,
-                      reload_data=False)
-
-    selectedTeam = '-'
-    if len(ag_teams['selected_rows']) > 0:
-        selectedTeam = ag_teams['selected_rows'][0]['Teams']
+                      fit_columns_on_grid_load=True)
 
 with secondRow[1]:
-    gb_players = GridOptionsBuilder.from_dataframe(
-        players[
-            ['_', selectedTeam] if selectedTeam != '-' else ['_']
-        ])
+    gb_players = GridOptionsBuilder.from_dataframe(players)
     gb_players.configure_default_column(editable=True)
-    gb_players.configure_selection(selection_mode='single',
-                                   use_checkbox=True)
 
     ag_players = AgGrid(players,
                         gridOptions=gb_players.build(),
                         fit_columns_on_grid_load=True)
 
-    selectedPlayer = '-'
-    if len(ag_players['selected_rows']) > 0:
-        selectedPlayer = ag_players['selected_rows'][0][selectedTeam]
-
 with secondRow[2]:
-    actions = pd.read_csv('data/actions.csv')
-    gb_actions = GridOptionsBuilder.from_dataframe(actions)
-    gb_actions.configure_default_column(editable=False)
-    gb_actions.configure_selection(selection_mode='single',
-                                   use_checkbox=True)
-    ag_actions = AgGrid(data=actions,
-                        gridOptions=gb_actions.build(),
-                        fit_columns_on_grid_load=True,
-                        update_on="selectionChanged")
+    events = pd.read_csv('data/events.csv')
+    gb_events = GridOptionsBuilder.from_dataframe(events)
+    gb_events.configure_default_column(editable=True)
+    ag_events = AgGrid(data=events,
+                       gridOptions=gb_events.build(),
+                       fit_columns_on_grid_load=True)
 
-    selectedAction = '-'
-    if len(ag_actions['selected_rows']) > 0:
-        selectedAction = ag_actions['selected_rows'][0]['Actions']
+with secondRow[3]:
+    saveChanges = st.button('Save changes')
+    if saveChanges:
+        updatedTeams = list(ag_teams['data']['Teams'])
+        updatedPlayers = ag_players['data']
+        updatedPlayers.columns = [updatedPlayers.columns[0]] + updatedTeams
+        updatedPlayers.to_csv('data/players.csv', index=False)
+        updatedEvents = ag_events['data']
+        updatedEvents.to_csv('data/events.csv', index=False)
 
 with secondRow[4]:
-    annotations = pd.read_csv('data/annotations.csv')
     gb_annotations = GridOptionsBuilder.from_dataframe(annotations)
     gb_annotations.configure_default_column(editable=True)
     ag_annotations = AgGrid(data=annotations,
                             gridOptions=gb_annotations.build(),
                             fit_columns_on_grid_load=True)
 
-with secondRow[3]:
-    submitAnnotation = st.button('submit annotation')
-    if submitAnnotation:
-        newAnnotation = pd.DataFrame({
-            annotations.columns[0]: [selectedTeam],
-            annotations.columns[1]: [selectedPlayer],
-            annotations.columns[2]: [selectedAction],
-            annotations.columns[3]: [secondsOfVideoPlayed // 60],
-            annotations.columns[4]: [secondsOfVideoPlayed % 60],
-            annotations.columns[5]: ['-'],
-            annotations.columns[6]: ['-'],
-            annotations.columns[7]: ['-'],
-            annotations.columns[8]: ['-']
-        })
-        pd.concat([ag_annotations.data, newAnnotation]).to_csv('data/annotations.csv')
-
 with firstRow[1]:
-    # @st.experimental_singleton
-    def get_cv2_video(url):
-        ydl = YoutubeDL()
-        video_data = ydl.extract_info(url, download=False)
-
-        # link with video and audio
-        direct_video_url = [_format['url'] for _format in video_data['formats']
-                            if _format['acodec'] != 'none' and _format['vcodec'] != 'none'][-1]
-
-        capture = cv2.VideoCapture(direct_video_url)
-
-        return capture
-
-
     if 'capturedVideo' not in st.session_state:
-        st.session_state['capturedVideo'] = get_cv2_video(videoURL)
+        if videoSourceType == 'URL':
+            ydl = YoutubeDL()
+            video_data = ydl.extract_info(videoURL, download=False)
+
+            # link with video and audio
+            direct_video_url = [_format['url'] for _format in video_data['formats']
+                                if _format['acodec'] != 'none' and _format['vcodec'] != 'none'][-1]
+
+            capture = cv2.VideoCapture(direct_video_url)
+        elif videoSourceType == 'File':
+            temporaryFile = NamedTemporaryFile(delete=False)
+            temporaryFile.write(videoFile.read())
+            capture = cv2.VideoCapture(temporaryFile.name)
+        st.session_state['capturedVideo'] = capture
+
     capturedVideo = st.session_state['capturedVideo']
 
 
@@ -152,18 +170,46 @@ with firstRow[1]:
         capturedVideo.set(cv2.CAP_PROP_POS_MSEC, played * 1000)
         check, frame = capturedVideo.read()
         if check:
+            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
             return Image.fromarray(frame)
         else:
+            st.error('Could not get current frame.')
             return None
 
 
-    canvas_frame = st_canvas(
-        fill_color="rgba(255, 165, 0, 0.3)",  # Fixed fill color with some opacity
-        background_image=get_frame(secondsOfVideoPlayed),
-        update_streamlit=True,
-        drawing_mode='polygon',
-        stroke_width=3
-    )
+    if annotationType != 'Event annotation':
+        if annotationType == 'Object annotation':
+            selectedTeam = st.selectbox(
+                'Choose team',
+                ['-'] + list(players.columns[[1, 2]]))
+            selectedPlayer = st.selectbox(
+                'Choose player',
+                players[selectedTeam] if selectedTeam != '-' else ['Ball'])
 
-# updated = ag_players['data']
-# updated.to_csv('data/players.csv', index=False)
+        currentFrame = get_frame(secondsOfVideoPlayed)
+        canvas_frame = st_canvas(
+            fill_color="rgba(255, 165, 0, 0.3)",  # Fixed fill color with some opacity
+            background_image=currentFrame,
+            update_streamlit=True,
+            drawing_mode=canvasDrawingMode,
+            height=videoHeight,
+            width=videoHeight * (currentFrame.width / currentFrame.height) if currentFrame else 1.5,
+            stroke_width=3
+        )
+    elif annotationType == 'Event annotation':
+        selectedEvent = st.selectbox(
+            'Choose event',
+            ['-'] + list(events['Actions']))
+        selectedTeam = st.selectbox(
+            'Choose team',
+            ['-'] + list(players.columns[[1, 2]]))
+        selectedPlayer = st.selectbox(
+            'Choose player',
+            ['-'] + list(players[selectedTeam]) if selectedTeam != '-' else ['-'])
+        submitAnnotation = st.button('Add annotation')
+        if submitAnnotation:
+            presentedAnnotations.add_rows({'Event': [selectedEvent],
+                                           'Team': [selectedTeam],
+                                           'Player': [selectedPlayer],
+                                           'Min': [secondsOfVideoPlayed // 60],
+                                           'Sec': [round(secondsOfVideoPlayed % 60, 2)]})
