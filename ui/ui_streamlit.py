353fc74c48d6d727f13b23947ba830bef7046063
kraszewskik <konstanty.kraszewski.stud@pw.edu.pl>
2022-12-31 19:56:48 +0100

    fix canvas width

--- ui/ui_streamlit.py
+++ ui/ui_streamlit.py
@@ -12,7 +12,7 @@ import cv2
 from base64 import b64encode
 import json
 from tempfile import NamedTemporaryFile
-from execute_scrapper import run_script
+# from execute_scrapper import run_script
 
 # streamlit configs
 st.set_page_config(
@@ -101,10 +101,10 @@ with sidebar:
         scrapData = st.form_submit_button('Get data')
         if scrapData:
             # initialize data scraping
-            try:
-                run_script(matchDate, firstTeam, secondTeam)
-            except:
-                st.error('No data could be found')
+            # try:
+            #     run_script(matchDate, firstTeam, secondTeam)
+            # except:
+            #     st.error('No data could be found')
 
             st.session_state['scrapedData'] = json.load(
                 open(matchDirectory + '/scrapped_data.json')
@@ -432,7 +432,7 @@ with firstRow[1]:
 
         currentFrame = get_frame(secondsOfVideoPlayed)
         frameHeight = videoHeight + 300
-        frameWidth = videoHeight * (currentFrame.width / currentFrame.height) if currentFrame else 1.5
+        frameWidth = frameHeight * (currentFrame.width / currentFrame.height) if currentFrame else 1.5
         scaleWidth = frameWidth / currentFrame.width
         scaleHeight = frameHeight / currentFrame.height
         teamsColors = {
