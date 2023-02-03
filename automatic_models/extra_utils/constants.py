1e8f8d5eecacc8176ba7e81eece7a41badfcd22f
skoczna_panda <michal.piasecki0@gmail.com>
2023-02-03 13:10:13 +0100

    updated direction for constants

--- automatic_models/extra_utils/constants.py
+++ automatic_models/extra_utils/constants.py
@@ -1,7 +1,7 @@
 from pathlib import Path
-
-PATH_TO_AUTOMATIC_MODELS = list(Path('/home').glob('**/BSc-soccer-annotator/automatic_models'))
+import os
+PATH_TO_AUTOMATIC_MODELS = list(Path('./../../..').glob('**/BSc-soccer-annotator/automatic_models'))
 if len(PATH_TO_AUTOMATIC_MODELS) != 1:
     raise Exception('Not explicit location of automatic models package')
 else:
-    PATH_TO_AUTOMATIC_MODELS = str(PATH_TO_AUTOMATIC_MODELS[0])
+    PATH_TO_AUTOMATIC_MODELS = str(PATH_TO_AUTOMATIC_MODELS[0].resolve())
